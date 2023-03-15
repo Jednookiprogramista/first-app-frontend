8d650569e208bd1d19ddaffdc1a047911ef81d12
Your Name <bvbrunik@gmail.com>
2023-03-15 22:40:42 +0100

    Added header structure and styled it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
+import './App.css'
 
 export const  App = () =>  {
-
+    useEffect(() => {
+        document.title = "MyWall";
+    }, []);
 
   return (
     <>
+        <header>
+        <h1>
+            <strong>MyWall</strong>
+        </h1>
+            <div className="myprofile" >
+                 <button>My profile</button>
 
+            </div>
+      </header>
     </>
   );
 };
