c1708fe1aa3283fa518ace397f443037579b420b
Your Name <bvbrunik@gmail.com>
2023-03-22 17:40:40 +0100

    Made a Register page

--- src/components/layout/register/Register.tsx
+++ src/components/layout/register/Register.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './Register.css.css'
+import './Register.css'
 
 export const Register = () => {
 
@@ -8,30 +8,32 @@ export const Register = () => {
         <div className={"register"}>
             <div className="card">
                 <div className="left">
-                    <h1>Welcome on <strong>MyWall</strong></h1>
+                    <h1>Your place on the internet!</h1>
+                    <br/>
 
                     <p>
-                        Hello everyone! <br/><br/>
-                        Welcome to my social media page. <br/>I am thrilled to have you all here with me.<br/>
-                        This platform is a
-                        place where we can connect, share ideas, and support each other. Let's build a positive and engaging community together. <br/>
-                        Feel free to comment,
-                        like and share your thoughts. I am excited to get to know each and every one of you.<br/><br/>
-                        Thank you for being here, and let's have some fun!
+
+                        Connect with us to stay up-to-date on our latest products, promotions, and events.<br/><br/>
+                        Engage with our community by sharing your thoughts, ideas, and feedback with us.<br/><br/>
+
                     </p>
-                    <span>Don't you have an account?</span>
-                    <button>Register</button>
+                    <span>Do you have an account?</span>
+                    <button>Login</button>
                 </div>
                 <div className="right">
-                    <h1>Login</h1>
+                    <h1>Register</h1>
                     <form>
                         <input type={"text"} placeholder={"Username"}/>
+                        <input type={"email"} placeholder={"Email"}/>
+                        <input type={"text"} placeholder={"Name"}/>
                         <input type={"password"} placeholder={"Password"}/>
                         <br/><br/>
-                        <button>Login</button>
+                        <button>Register</button>
                     </form>
                 </div>
             </div>
         </div>
     )
 }
+
+
