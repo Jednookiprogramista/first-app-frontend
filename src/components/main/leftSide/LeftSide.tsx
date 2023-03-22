95e93f0bd6bbbd4ceac60cc58c946a9d2394c470
Your Name <bvbrunik@gmail.com>
2023-03-22 20:45:55 +0100

    Created files to make the base of my app

---
+++ src/components/main/leftSide/LeftSide.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './leftSide.css'
+
+
+export const LeftSide = () => {
+
+    return (
+        <>
+
+
+        </>
+
+
+    )
+}
---
+++ src/components/main/rightSide/RightSide.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './rightSide.css'
+
+
+export const RightSide = () => {
+
+    return (
+        <>
+
+
+        </>
+
+
+    )
+}
