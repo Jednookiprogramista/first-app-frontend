db97a38fa9790bc58b4a3079c99f12459e8930c4
Your Name <bvbrunik@gmail.com>
2023-04-04 16:21:13 +0200

    Added 3 dots to post and fixed contribution of div

--- src/components/main/post/Post.tsx
+++ src/components/main/post/Post.tsx
@@ -6,6 +6,7 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import CommentIcon from '@mui/icons-material/Comment';
 import SendIcon from '@mui/icons-material/Send';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 interface PostProps {
     post: PostBuild;
@@ -17,18 +18,23 @@ export const Post = ({ post }: PostProps) => {
     return (
         <div className="post">
             <div className="container__">
-            <div className="user__"></div>
+                <div className="user__">
             <div className="userInformation">
                 <img  className={'profile-image__'} src={post.profilePicture} alt={'author post'}/>
                 <div className="details">
+
                     <Link to={`profile/${post.userId}`} className={'main-user'} >
                         <span className={'name__'}>{post.name}</span>
 
                     </Link>
                     <span className={'time__'}> 5 min ago</span>
                 </div>
+
             </div>
+                    <MoreHorizIcon/>
+                </div>
             <div className="content__">
+
                 <p className={'description'}>{post.description}</p>
                 <img className={'.image-post'} src={post.image}  alt={'User content'}/>
             </div>
@@ -39,6 +45,7 @@ export const Post = ({ post }: PostProps) => {
                     <SendIcon/> 30
                 </div>
             </div>
+
             </div>
         </div>
     );
